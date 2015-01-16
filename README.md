7870e7a1b5665e971a69cd176f16b24b33e01dd2
Patrik Skoog <patrik.skoog@gmail.com>
2015-01-16 14:39:27 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,60 @@
+Springworks Yeoman Generator
+============================
+
+[![Build Status](https://travis-ci.org/Springworks/generator-springworks.png?branch=master)](https://travis-ci.org/Springworks/generator-springworks)
+[![Coverage Status](https://coveralls.io/repos/Springworks/generator-springworks/badge.png?branch=master)](https://coveralls.io/r/Springworks/generator-springworks?branch=master)
+
+This generator helps to setup new projects in the Springworks organization.
+
+And yes, it will add badges and license to the `README.md` file for public projects.
+
+## How do I use it?
+First of all you need the `yo` cli tool to run the generators.
+
+```bash
+$ npm install -g yo
+```
+
+When the cli tool is installed it's time to install the generator.
+
+```bash
+$ npm install -g generator-springworks
+```
+
+Once all the required packages are installed you can run
+
+```bash
+$ mkdir my-awesome-project && cd my-awesome-project
+$ yo springworks
+```
+
+There are a couple of options for the generators, and those can be listed with
+
+```bash
+$ yo springworks --help
+```
+
+## What does it do?
+The generator will ask you a couple of questions and then setup the project for you.
+
+The files created are the following
+
+```
+├─── package.json
+├─── README.md
+├─── LICENSE (Only in public projects)
+├─── .gitignore
+├─── .gitattributes
+├─── .eslintignore
+├─── .eslintrc
+├─── .gjslintrc
+├─── .npmignore
+├─── .travis.yml (Only in public projects)
+└─── test/
+     └─── .eslintrc
+```
+
+
+## License
+
+MIT
---
+++ generators/app/index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var yeoman = require('yeoman-generator'),
+    chalk = require('chalk');
+
+
+
+module.exports = yeoman.generators.Base.extend({
+
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+    this.option('skip-install', {desc: 'Skip npm install'});
+  },
+
+  initializing: function () {
+    var welcomeMessage = [
+          '\nSpringworks Scaffolding Generator\n'
+        ];
+    this.log(welcomeMessage.join(''));
+    this.currentYear = (new Date()).getFullYear();
+  },
+
+  // Ask the user a couple of questions
+  prompting: function () {
+    var done = this.async(),
+
+        prompts = [
+          {
+            type: 'input',
+            name: 'projectName',
+            message: 'What\'s your project name?',
+            default: this.appname
+          },
+          {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Whats\'s your project description?',
+            default: this.appname
+          },
+          {
+            type: 'list',
+            name: 'projectVisibility',
+            message: 'Is this a private or public project?',
+            choices: [
+              {
+                name: 'Private',
+                value: 'privateProject'
+              },
+              {
+                name: 'Public',
+                value: 'publicProject'
+              }
+            ],
+            default: 0
+          }
+        ];
+
+    this.prompt(prompts, function (answers) {
+      this.appname = answers.projectName.toLowerCase();
+      this.projectDescription = answers.projectDescription;
+      this.privateProject = answers.projectVisibility === 'privateProject' ? true : false;
+      done();
+    }.bind(this));
+
+  },
+
+  // Writes configuration to the `.yo-rc.json` file
+  configuring: function () {
+    this.config.set({
+      projectName: this.appname,
+      projectDescription: this.projectDescription,
+      privateProject: this.privateProject
+    });
+  },
+
+  // Writes a bunch of files to the destination directory
+  writing: {
+    projectfiles: function () {
+      this.template('_package.json', 'package.json');
+      this.template('_README.md', 'README.md');
+
+      if (!this.privateProject) { // Extra files for public projects
+        this.copy('travis.yml', '.travis.yml');
+        this.template('_LICENSE', 'LICENSE');
+      }
+    },
+
+    gitFiles: function() {
+      this.copy('gitignore', '.gitignore');
+
+      if (this.privateProject) { // Extra files for private projects
+        this.copy('gitattributes', '.gitattributes');
+      }
+    },
+
+    lintFiles: function() {
+      this.copy('eslintignore', '.eslintignore');
+      this.copy('eslintrc', '.eslintrc');
+      this.copy('test/eslintrc', 'test/.eslintrc');
+      this.copy('gjslintrc', '.gjslintrc'); // Skoog specific ;)
+    },
+
+    npmFiles: function() {
+      this.copy('npmignore', '.npmignore');
+    }
+  },
+
+  // Installs dependencies
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install'],
+      bower: false
+    });
+  }
+
+});
