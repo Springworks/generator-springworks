57bd0f1070019ba0d86351a7c428003f76537c68
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2016-03-01 12:35:50 +0100

    Validate Swagger spec when generating file

--- generators/static-api/templates/bin/generate-api-file.js
+++ generators/static-api/templates/bin/generate-api-file.js
@@ -8,9 +8,68 @@
 
 const fs = require('fs');
 const path = require('path');
+const swagger_tools = require('swagger-tools');
 const api_spec = require('../resources/api/index');
 const target_filename = path.join(__dirname, '..', 'packages', 'static-api', 'api.json');
 
-const api_as_json = JSON.stringify(api_spec, null, 2) + '\n';
+const internals = {
 
-fs.writeFileSync(target_filename, api_as_json);
+  validateSwaggerSpec(swagger_api, callback) {
+    const swagger_spec = swagger_tools.specs.v2;
+    swagger_spec.validate(swagger_api, (err, result) => {
+      if (err) {
+        callback(err);
+        return;
+      }
+
+      if (!result) {
+        callback(null, null);
+        return;
+      }
+
+      if (!result.errors.length && !result.warnings.length) {
+        callback(null, null);
+        return;
+      }
+
+      if (result.errors.length > 0) {
+        console.log('Invalid Swagger spec:');
+        console.log('');
+        console.log('Errors');
+        console.log('------');
+        result.errors.forEach(function(err) {
+          console.log('#/' + err.path.join('/') + ': ' + err.message);
+        });
+        console.log('');
+      }
+
+      if (result.warnings.length > 0) {
+        console.log('Warnings');
+        console.log('--------');
+        result.warnings.forEach(function(warn) {
+          console.log('#/' + warn.path.join('/') + ': ' + warn.message);
+        });
+        console.log('');
+      }
+
+      const error = new Error('Swagger spec has issues');
+      callback(error, null);
+    });
+  },
+
+  writeSwaggerFile(swagger_api) {
+    const api_as_json = JSON.stringify(swagger_api, null, 2) + '\n';
+    fs.writeFileSync(target_filename, api_as_json);
+  },
+
+};
+
+internals.validateSwaggerSpec(api_spec, err => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+    return;
+  }
+
+  internals.writeSwaggerFile(api_spec);
+});
